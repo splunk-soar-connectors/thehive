0a3fa5ab04f4606877035d7026fcedccb0f6ec62
Ishan Ghelani <ighelani@splunk.com>
2022-04-19 11:15:27 +0530

    TheHive: Including connector/json checklist changes

--- thehive_view.py
+++ thehive_view.py
@@ -21,10 +21,10 @@ def get_ctx_result(result):
 
     ctx_result['param'] = param
 
-    if (data):
+    if data:
         ctx_result['data'] = data
 
-    if (summary):
+    if summary:
         ctx_result['summary'] = summary
 
     return ctx_result
@@ -37,8 +37,8 @@ def display_get_observables(provides, all_app_runs, context):
         for result in action_results:
 
             ctx_result = get_ctx_result(result)
-            if (not ctx_result):
+            if not ctx_result:
                 continue
             results.append(ctx_result)
-    # print context
+
     return 'thehive_display_get_observables.html'
